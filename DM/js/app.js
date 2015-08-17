ca6e1790a9c38d9324e7a01b275c7bcaca8b709c
pr0dd <sgx4321@gmail.com>
2015-08-17 15:21:44 +0300

    angular SPA's pre-setup is done

--- DM/js/app.js
+++ DM/js/app.js
@@ -4,3 +4,34 @@ var app = angular.module("app", [
 	"appDirs", 
 	"appCtrls"
 ]);
+
+app.config(["$routeProvider", function($routeProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "partials/home.html",
+			controller: "homeCtrl"
+		})
+		.when("/services", {
+			templateUrl: "partials/services.html",
+			controller: "servicesCtrl"
+		})
+		.when("/work", {
+			templateUrl: "partials/work.html",
+			controller: "workCtrl"
+		})
+		.when("/about", {
+			templateUrl: "partials/about.html",
+			controller: "aboutCtrl"
+		})
+		.when("/contacts", {
+			templateUrl: "partials/contacts.html",
+			controller: "contactsCtrl"
+		})
+		.when("/blog", {
+			templateUrl: "partials/blog.html",
+			controller: "blogCtrl"
+		})
+		.otherwise({
+			redirectTo: "/"
+		});
+}]);
--- DM/js/controllers.js
+++ DM/js/controllers.js
@@ -10,3 +10,27 @@ var appCtrls = angular.module("appCtrls", []);
 // 	},
 // 	$scope.load();
 // }]);
+
+appCtrls.controller("homeCtrl", ["$scope", function($scope){
+	$scope.foo = "home-foo";
+}]);
+
+appCtrls.controller("servicesCtrl", ["$scope", function($scope){
+	$scope.foo = "services-foo";
+}]);
+
+appCtrls.controller("workCtrl", ["$scope", function($scope){
+	$scope.foo = "work-foo";
+}]);
+
+appCtrls.controller("aboutCtrl", ["$scope", function($scope){
+	$scope.foo = "about-foo";
+}]);
+
+appCtrls.controller("contactsCtrl", ["$scope", function($scope){
+	$scope.foo = "contacts-foo";
+}]);
+
+appCtrls.controller("blogCtrl", ["$scope", function($scope){
+	$scope.foo = "blog-foo";
+}]);
