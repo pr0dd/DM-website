bd46a9124424140c44b41c5170ff3ecaf272fbd8
pr0dd <sgx4321@gmail.com>
2015-08-18 09:18:01 +0300

    home.html is ready

--- DM/js/controllers.js
+++ DM/js/controllers.js
@@ -1,18 +1,48 @@
 var appCtrls = angular.module("appCtrls", []);
 
-// appCtrls.controller("topmenu", ["$scope", function($scope){
-// 	$scope.load = function(){
-// 		var button = $("button.pop"),
-// 				hiddenList = button.next("ul");
-// 		button.on("click", function(){
-// 			hiddenList.fadeToggle();
-// 		});
-// 	},
-// 	$scope.load();
-// }]);
-
 appCtrls.controller("homeCtrl", ["$scope", function($scope){
 	$scope.foo = "home-foo";
+	$scope.slider = function(){
+		//settings for slider
+    var width = 100,
+    		animationSpeed = 800,
+    		pause = 3500,
+    		currentSlide = 1;
+
+    //cache DOM elements
+    var slider = $('.slider'),
+    		slideContainer = $('.move', slider),
+    		slides = $('img', slider);
+
+    //initialize interval
+    var interval;
+    
+    //start function
+    function startSlider() {
+      interval = setInterval(function() {
+        slideContainer.animate({'left': '-='+width+"%"}, animationSpeed, function() {
+          if (++currentSlide === slides.length) {
+            currentSlide = 1;
+            slideContainer.css('left', 0+"%");
+          }
+        });
+      }, pause);
+    }
+    
+    //pause function
+    function pauseSlider() {
+      clearInterval(interval);
+    }
+    
+    //set event handlers
+    slideContainer
+      .on('mouseenter', pauseSlider)
+      .on('mouseleave', startSlider);
+
+    startSlider();
+	}
+	$scope.slider();
+
 }]);
 
 appCtrls.controller("servicesCtrl", ["$scope", function($scope){
